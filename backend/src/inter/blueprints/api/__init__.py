0e9ec44dad2d45a44a9f2b13944117015e81ab0b
Matthew Li <matthew.li10@education.nsw.gov.au>
2026-02-11 15:04:34 +1100

    Use httpx AsyncClient to avoid blocking the event loop, refactor emote set enumeration

--- backend/src/inter/blueprints/api/__init__.py
+++ backend/src/inter/blueprints/api/__init__.py
@@ -140,36 +140,46 @@ GLOBAL_EMOTE_SET_QUERY = """
 
 @api.route("/emotes", methods=["GET"])
 async def _():
-    return quart.jsonify(
-        {
-            emote["name"]: (
-                next(
-                    image["url"].replace(
-                        "https://cdn.7tv.app/",
-                        "https://em.circles-png.workers.dev/cdn.7tv.app/",
-                    )
-                    for image in emote["emote"]["images"]
-                    if image["url"].endswith("2x.webp")
-                ),
-                emote["emote"]["flags"]["zeroWidth"],
-            )
-            for emote in [
-                *httpx.post(
-                    f"https://em.circles-png.workers.dev/7tv.io/v4/gql",
-                    json={
-                        "query": CUSTOM_EMOTE_SET_QUERY,
-                        "variables": {"set": environ["EMOTE_SET"]},
-                    },
-                ).json()["data"]["emoteSets"]["emoteSet"]["emotes"]["items"],
-                *httpx.post(
-                    f"https://em.circles-png.workers.dev/7tv.io/v4/gql",
-                    json={
-                        "query": GLOBAL_EMOTE_SET_QUERY,
-                    },
-                ).json()["data"]["emoteSets"]["global"]["emotes"]["items"],
-            ]
-        }
-    )
+    async with httpx.AsyncClient() as client:
+        return quart.jsonify(
+            {
+                emote["name"]: (
+                    next(
+                        image["url"].replace(
+                            "https://cdn.7tv.app/",
+                            "https://em.circles-png.workers.dev/cdn.7tv.app/",
+                        )
+                        for image in emote["emote"]["images"]
+                        if image["url"].endswith("2x.webp")
+                    ),
+                    emote["emote"]["flags"]["zeroWidth"],
+                )
+                for set in [
+                    (
+                        await client.post(
+                            f"https://em.circles-png.workers.dev/7tv.io/v4/gql",
+                            json=body,
+                        )
+                    ).json()["data"]["emoteSets"][key]["emotes"]["items"]
+                    for body, key in [
+                        (
+                            {
+                                "query": CUSTOM_EMOTE_SET_QUERY,
+                                "variables": {"set": environ["EMOTE_SET"]},
+                            },
+                            "emoteSet",
+                        ),
+                        (
+                            {
+                                "query": GLOBAL_EMOTE_SET_QUERY,
+                            },
+                            "global",
+                        ),
+                    ]
+                ]
+                for emote in set
+            }
+        )
 
 
 api.register_blueprint(stream)
