d8703b1d2dbfe0eb6eb0c15a9738307849d61181
Matthew Li <matthew.li10@education.nsw.gov.au>
2026-05-04 09:20:57 +1000

    Update homepage and update postcss to fix vulnerability

--- frontend/src/routes/(main)/+page.svelte
+++ frontend/src/routes/(main)/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+  import { invalidateAll } from "$app/navigation"
   import { resolve } from "$app/paths"
   import { Empty, EmptyDescription, EmptyHeader, EmptyTitle } from "$lib/components/ui/empty"
   import { server } from "$lib/utils.svelte.js"
+  import { onMount } from "svelte"
   let { data } = $props()
   let streams = $derived(data.streams)
+  onMount(() => {
+    const interval = setInterval(async () => {
+      await invalidateAll()
+    }, 5000)
+    return () => {
+      clearInterval(interval)
+    }
+  })
 </script>
 
 <main class="p-2 flex flex-col gap-2 overflow-y-auto grow pr-0">
@@ -13,7 +23,11 @@
       href={resolve("/(main)/@[username=user]/watch", { username })}
       data-sveltekit-reload
     >
-      <img src={server.user.streamPreview(username)} alt={title} class="w-80 rounded-md" />
+      <img
+        src={server.user.streamPreview(username)}
+        alt={title}
+        class="w-80 rounded-md aspect-video"
+      />
       <div class="flex flex-col">
         <div class="font-bold">{title}</div>
         <div class="text-sm text-muted-foreground">
